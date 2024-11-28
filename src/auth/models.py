98a7cd9601e2ec794185e9e2d9ed281bde2a6e99
OLoghunov <loghunov1@gmail.com>
2024-11-28 23:16:16 +0300

    Added users table via alembic

---
+++ src/auth/models.py
@@ -0,0 +1,22 @@
+from sqlmodel import SQLModel, Field, Column
+from datetime import datetime
+import sqlalchemy.dialects.postgresql as pg
+import uuid
+
+
+class UserModel(SQLModel, table=True):
+    __tablename__ = 'users'
+    
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    username: str
+    email: str
+    firstName: str
+    lastName: str
+    isVerified: bool = Field(default=False)
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    
+    def __repr__(self):
+        return f"<User {self.username}>"
