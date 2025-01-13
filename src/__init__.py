759a903bc3691fa2735563b9c5744cab086c4fef
OLoghunov <loghunov1@gmail.com>
2025-01-13 22:45:50 +0300

    registered reviews router

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from src.books.routes import booksRouter
 from src.auth.routes import authRouter
+from src.reviews.routes import reviewRouter
 from contextlib import asynccontextmanager
 from src.db.main import initDb
 
@@ -23,3 +24,4 @@ app = FastAPI(
 
 app.include_router(booksRouter, prefix="/api/{version}/books", tags=["books"])
 app.include_router(authRouter, prefix="/api/{version}/auth", tags=["auth"])
+app.include_router(reviewRouter, prefix="/api/{version}/reviews", tags=["reviews"])
