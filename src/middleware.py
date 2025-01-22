01b4d39c4ae32a99e3a68a7419a20ef8a7efda77
OLoghunov <loghunov1@gmail.com>
2025-01-22 21:12:52 +0300

    added CORS middleware

--- src/middleware.py
+++ src/middleware.py
@@ -4,6 +4,8 @@ import logging
 from fastapi import FastAPI, status
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 
 logger = logging.getLogger("uvicorn.access")
@@ -23,3 +25,13 @@ def registerMiddleware(app: FastAPI):
         print(message)
 
         return response
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials=True,
+    )
+
+    app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1"])
